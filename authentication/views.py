79b37c00f67c827d10377ef901d990d93140a017
Bruno Roberto Carvalho <brc@neo.ufsc.br>
2016-09-21 23:58:40 -0300

    Created file for header filling function, with language verification.

--- authentication/views.py
+++ authentication/views.py
@@ -11,13 +11,18 @@ def userLogin(request):
 
         if user is not None:
             login(request, user)
-            #Verify if user is Embraco's employee
+
+            #Verify user type: Supplier or Embraco's user
             embraco_user = EmbracoProfile.objects.get(user=user.id)
-            if embraco_user: #If is Embraco's user
+            supplier_user = EmbracoProfile.objects.get(user=user.id)
+            if embraco_user: #If it's Embraco's user
                 request.session['user_type'] = 'Embraco'
-            else:
-                supplier_user = SupplierProfile.objects.get(user=user.id)
+            elif supplier_user: #If it's a Supplier's user
                 request.session['user_type'] = 'Supplier'
+            else:
+                request.session['user_type'] = 'Undefined'
+            #End user type verification
+
             if 'next' in request.GET:
                 return HttpResponseRedirect(request.GET['next'])
             else:

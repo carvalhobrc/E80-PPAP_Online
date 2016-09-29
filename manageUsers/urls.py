0e15189a8a537bd1be4c43c653977649a699f06c
Bruno Roberto Carvalho <brc@neo.ufsc.br>
2016-09-29 20:57:13 -0300

    manageUsers form is saving correctly. Need to instance the model for the modelForm editing.

--- manageUsers/urls.py
+++ manageUsers/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.views.generic import DetailView
 from authentication.models import *
 from . import views
 
 urlpatterns = [
     url(r'^$', views.manageUsers, name = 'manage-users'),
-    url(r'^embraco/(?P<pk>\d+)$', DetailView.as_view(model=EmbracoProfile, template_name='manageUsers/embraco_user_detail.html')),
-    url(r'^supplier/(?P<pk>\d+)$', DetailView.as_view(model=SupplierProfile, template_name='manageUsers/supplier_user_detail.html')),
+    url(r'^embraco/new/$', views.editEmbracoUser, name = 'new-user'),
+    url(r'^supplier/new/$', views.editSupplierUser, name = 'new-supplier'),
+    url(r'^embraco/(?P<pk>\d+)$', views.editEmbracoUser, name = 'edit-user-info'),
+    url(r'^supplier/(?P<pk>\d+)$', views.editSupplierUser, name = 'edit-supplier-info'),
 ]
--- manageUsers/views.py
+++ manageUsers/views.py
@@ -5,35 +5,66 @@ from django.http import HttpResponseRedirect
 
 
 def manageUsers(request):
+    if request.session['user_type'] == 'Embraco':
+        embraco_users = EmbracoProfile.objects.all()
+    else:
+        embraco_users = None
+    supplier_users = SupplierProfile.objects.all()
+
+    return render(request,
+                  'manageUsers/manage-users.html',
+                  {
+                      "embraco_users": embraco_users,
+                      "supplier_users": supplier_users,
+                  })
+
+def editEmbracoUser(request):
+    instance = User.objects.get(id=2)
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        user_form = UserForm(request.POST)
-        embraco_form = EmbracoUserForm(request.POST)
+        user_form = UserForm(request.POST, instance=instance)
+        embraco_form = EmbracoUserForm(request.POST, instance=instance.embracoprofile)
         # check whether it's valid:dj
         if user_form.is_valid() and embraco_form.is_valid():
             # process the data in form.cleaned_data as required
             created_user = user_form.save()
-            embraco_form.cleaned_data['user'] = created_user.pk
-            embraco_form.save()
+            created_embraco_user = embraco_form.save(commit=False)
+            created_embraco_user.user = created_user
+            created_embraco_user.save()
             # redirect to a new URL:
             return HttpResponseRedirect('manage-users/')
-
-            # if a GET (or any other method) we'll create a blank form
     else:
-        if request.session['user_type'] == 'Embraco':
-            embraco_users = EmbracoProfile.objects.all()
-        else:
-            embraco_users = None
-        supplier_users = SupplierProfile.objects.all()
-
         user_form = UserForm
         embraco_user_form = EmbracoUserForm
 
         return render(request,
-                      'manageUsers/manage-users.html',
+                      'manageUsers/embraco_user_edit.html',
                       {
-                          "embraco_users": embraco_users,
-                          "supplier_users": supplier_users,
                           "user_form": user_form,
                           "embraco_user_form": embraco_user_form,
+                      })
+
+def editSupplierUser(request):
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        user_form = UserForm(request.POST)
+        supplier_form = SupplierUserForm(request.POST)
+        # check whether it's valid:dj
+        if user_form.is_valid() and supplier_form.is_valid():
+            # process the data in form.cleaned_data as required
+            created_user = user_form.save()
+            created_supplier_user = supplier_form.save(commit=False)
+            created_supplier_user.user = created_user
+            created_supplier_user.save()
+            # redirect to a new URL:
+            return HttpResponseRedirect('manage-users/')
+    else:
+        user_form = UserForm
+        supplier_user_form = SupplierUserForm
+
+        return render(request,
+                      'manageUsers/manage-users.html',
+                      {
+                          "user_form": user_form,
+                          "embraco_user_form": supplier_user_form,
                       })
